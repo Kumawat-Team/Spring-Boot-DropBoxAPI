f09e280eac958ecedd8be9cc0ba693ebc1899933
kamleshkk <kamleshkk@webline.local>
2020-08-09 12:24:00 +0530

    git push

--- src/main/java/com/wli/dropboxapi/dropboxapi/config/Config.java
+++ src/main/java/com/wli/dropboxapi/dropboxapi/config/Config.java
@@ -5,7 +5,7 @@ import com.dropbox.core.v2.DbxClientV2;
 
 public class Config {
 
-	private static String ACCESS_TOKEN = "XYp7Dzrx03AAAAAAAAAAARbn1YncnLr5_bAEAgUSRh3Wlxc2wBazpLET2boh6uqC";
+	private static String ACCESS_TOKEN = "lXusybDkLNAAAAAAAAAAAcNMUKM7rBxkONown-i22ZPTqD9QwhcAKz5_-4b--Faz";
 
 	private static DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build();
 	public static DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
